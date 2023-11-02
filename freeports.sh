0b18fbdd48cea2066142c31fa21d55215e104d4c
Евгений Чащин <chadnis2010@inbox.ru>
2023-11-02 14:56:22 +0000

    already

--- freeports.sh
+++ freeports.sh
@@ -1,58 +1,48 @@
 #!/bin/bash
 
+function unique_array() {
+  # Объявляем переменную для хранения уникальных элементов
+    local -a unique_elements=()
+
+  # Проходим по каждому элементу массива
+    for element in "${@}"; do
+  # Если элемент не содержится в уникальных элементах, добавляем его
+        if [[ "${unique_elements[@]}" != *"$element"* ]]; then
+	      unique_elements+=("$element")
+        fi
+    done
+    echo "${unique_elements[*]}"
+   }
+	
 array=($(netstat -tupln | awk '{print $4}' | awk  -F":" '{print $NF}') )
 
-# Функция для удаления повторяющихся элементов
-removeDups() {
-    local duplicate_found=0
-    local new_array=()
-    local entry=${1}
-			    
-    for ((i=1; i<${#array[@]}; i++)); do
-    	if [[ "${array[$i]}" == "$entry" ]]; then
-    	duplicate_found=1
-    fi
-    done
-									        
-    if [[ $duplicate_found -eq 0 ]]; then
-    new_array+=("$entry")
-    fi
-    return ${duplicate_found}
-}
+sorted=()
 
-# Проходимся по массиву, вызывая функцию удаления дубликатов
-
-exit_array={}
-
-for ((i=0; i<${#array[@]}; i++ )); do
-	exit_array+=$(removeDup ${array[$i]})
-
+for i in $(unique_array "${array[@]}"); do
+	sorted+=("$i")
 done
 
-echo $exit_array
 
 #Сортировка массива для отсеивания строковых значений
-
 tmpArray=()
-for i in "${array[@]}"; do
+for i in "${sorted[@]}"; do
     if [[ "$i" =~ ^-?[0-9]+$ ]]; then
-            # Если элемент является числом...
+           # Если элемент является числом...
             tmpArray+=("$i")
     fi
 done
-
 array=("${tmpArray[@]}")
 
 #Сортировка числового массива
 
 IFS=$'\n'
-sorted=($(sort <<<"${array[*]}"));
+sorted_last=($(sort <<<"${array[*]}"));
 unset IFS
 
 
 # Вывод отсортированного массива
 
-for i in "${sorted[@]}"; do
+for i in "${sorted_last[@]}"; do
     	
     echo "$i"
 done
